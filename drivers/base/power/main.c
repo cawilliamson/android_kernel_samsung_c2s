80de4fae6eff844313492a033129f5932237a0c4
Christopher A. Williamson <home@chrisaw.com>
2021-03-04 10:14:37 +0000

    Add Boeffla 1.10 (with no default blocks)

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -720,6 +720,10 @@ void dpm_noirq_resume_devices(pm_message_t state)
 	struct device *dev;
 	ktime_t starttime = ktime_get();
 
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+	pm_print_active_wakeup_sources();
+#endif
+
 	trace_suspend_resume(TPS("dpm_resume_noirq"), state.event, true);
 	dbg_snapshot_suspend("dpm_resume_noirq", dpm_noirq_resume_devices,
 				NULL, state.event, DSS_FLAG_IN);
